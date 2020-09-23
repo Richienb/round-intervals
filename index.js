26c116d3ba5327d54eb37eba3a3b134f7c60edbe
Richie Bendall <richiebendall@gmail.com>
2020-09-23 21:36:07 +1200

    Add type checking
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 "use strict"
 
-module.exports = (number, intervals) => Math.round(number / intervals) * intervals
+module.exports = (number, intervals) => {
+    if (typeof number !== "number") {
+        throw new TypeError(`Expected a number, got ${typeof number}`)
+    }
+
+    if (typeof intervals !== "number") {
+        throw new TypeError(`Expected a number, got ${typeof intervals}`)
+    }
+
+    return Math.round(number / intervals) * intervals
+}
